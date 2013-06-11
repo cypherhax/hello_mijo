cec1b5c5a445e25536109b9977445cbefb49893b
Frank <frank@FrakIt.com>
2013-06-11 12:08:36 -0700

    Test 1

---
+++ test.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Frank
+#
+# Created:     11/06/2013
+# Copyright:   (c) Frank 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import os
+import platform
+print platform
+print os.name
+
+import sys
+print sys.platform
+
+print "hello"
+
